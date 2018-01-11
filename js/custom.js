1cf23533aea11ad29340953178883b08f0e6e218
Yun Nie <nieyun901021@gmail.com>
2018-01-11 05:33:08 -0500

    modified

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
 
 /* header */
 function openNav() {
@@ -10,12 +10,61 @@ function closeNav() {
 }
 
 
-/* ... */
 
 
 
+$(function() {
+    $('.project-overlay.one').on('click', function() {
+        $('.enlargeImageModalSource1').attr('src', $(this).attr('src'));
+        $('#enlargeImageModal1').modal('show');
+    });
+});
 
+$(function() {
+    $('.project-overlay.two').on('click', function() {
+        $('.enlargeImageModalSource2').attr('src', $(this).attr('src'));
+        $('#enlargeImageModal2').modal('show');
+    });
+});
 
+$(function() {
+    $('.project-overlay.three').on('click', function() {
+        $('.enlargeImageModalSource3').attr('src', $(this).attr('src'));
+        $('#enlargeImageModal3').modal('show');
+    });
+});
 
+$(function() {
+    $('.project-overlay.four').on('click', function() {
+        $('.enlargeImageModalSource4').attr('src', $(this).attr('src'));
+        $('#enlargeImageModal4').modal('show');
+    });
+});
 
-</script>
+$(function() {
+    $('.project-overlay.five').on('click', function() {
+        $('.enlargeImageModalSource5').attr('src', $(this).attr('src'));
+        $('#enlargeImageModal5').modal('show');
+    });
+});
+
+$(function() {
+    $('.project-overlay.six').on('click', function() {
+        $('.enlargeImageModalSource6').attr('src', $(this).attr('src'));
+        $('#enlargeImageModal6').modal('show');
+    });
+});
+
+$(function() {
+    $('.project-overlay.seven').on('click', function() {
+        $('.enlargeImageModalSource7').attr('src', $(this).attr('src'));
+        $('#enlargeImageModal7').modal('show');
+    });
+});
+
+$(function() {
+    $('.project-overlay.eight').on('click', function() {
+        $('.enlargeImageModalSource8').attr('src', $(this).attr('src'));
+        $('#enlargeImageModal8').modal('show');
+    });
+});
